8e293216fb6ac1f6e043cf63448c727b9786004a
i999rri <melody.hina.mk3@gmail.com>
2026-03-29 14:19:37 +0900

    Add Win32 entry point with message loop

---
+++ GhosttyWin32.cpp
@@ -0,0 +1,32 @@
+#include "framework.h"
+#include "GhosttyBridge.h"
+
+int APIENTRY wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+
+    // Initialize libghostty
+    auto& bridge = GhosttyBridge::instance();
+    if (!bridge.initialize()) {
+        MessageBoxW(nullptr, L"Failed to initialize Ghostty", L"Error", MB_OK);
+        return 1;
+    }
+
+    // Create surface (standalone Win32 window)
+    bridge.createSurface(nullptr);
+
+    // Message loop
+    MSG msg;
+    while (GetMessageW(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    return (int)msg.wParam;
+}
